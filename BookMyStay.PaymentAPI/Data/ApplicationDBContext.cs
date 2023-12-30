ddbad41a48aa986e9721608cef1f785b6137dbd8
nbaua <nhilesh.baua@gmail.com>
2023-12-30 12:01:12 +0530

    Added database context and Mapping classes

---
+++ BookMyStay.PaymentAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using BookMyStay.PaymentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookMyStay.PaymentAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<PaymentItem> PaymentItem { get; set; }
+        public DbSet<PaymentItemDetails> PaymentItemDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BookMyStay.PaymentAPI/Data/DataMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BookMyStay.PaymentAPI.Models;
+
+namespace BookMyStay.PaymentAPI.Data
+{
+    public class DataMapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<BookingItem, BookingItemDTO>().ReverseMap();
+                //config.CreateMap<BookingDetails, BookingDetailsDTO>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
