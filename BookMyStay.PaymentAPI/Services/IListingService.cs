e44ceb43d41cd89f38aed01b783854b5888cea8c
nbaua <nhilesh.baua@gmail.com>
2023-12-30 11:15:51 +0530

    Added listing service limited calls for payment API integration

---
+++ BookMyStay.PaymentAPI/Services/IListingService.cs
@@ -0,0 +1,9 @@
+﻿using BookMyStay.PaymentAPI.Models;
+
+namespace BookMyStay.PaymentAPI.Services
+{
+    public interface IListingService
+    {
+        Task<IEnumerable<ListingDTO>> GetListings();
+    }
+}
