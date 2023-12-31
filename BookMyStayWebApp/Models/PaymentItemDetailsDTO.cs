1935dab149e6d366ec557d1b891f5ce8f1024209
nbaua <nhilesh.baua@gmail.com>
2023-12-31 11:24:34 +0530

    Created Payment Item and Payment Item Details DTO

---
+++ BookMyStayWebApp/Models/PaymentItemDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BookMyStay.WebApp.Models
+{
+    public class PaymentItemDetailsDTO
+    {
+        public int PaymentItemDetailId { get; set; }
+        public int PaymentItemId { get; set; }
+        public int ListingId { get; set; }
+        public double BookingPrice { get; set; } //Amount at which the booking was done.
+        public ListingDTO? Listing { get; set; }
+        public int DayOfStay { get; set; }
+    }
+}
