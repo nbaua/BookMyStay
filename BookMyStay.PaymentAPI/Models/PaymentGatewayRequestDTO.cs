3c6b740541ba0b2174111285f1d4ffaa5b2a0df4
nbaua <nhilesh.baua@gmail.com>
2024-01-01 19:55:53 +0530

    Adding a DTO for a custom Payment Gateway Request (Stripe)

---
+++ BookMyStay.PaymentAPI/Models/PaymentGatewayRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMyStay.PaymentAPI.Models
+{
+    public class PaymentGatewayRequestDTO
+    {
+        //Stripe Payment Gateway Request Items
+        public string? SessionId { get; set; }
+        public string? SessionUrl { get; set; }
+        public string SuccessUrl { get; set; } //On Payment Success 
+        public string FailureUrl { get; set; } //On Error - Cancel
+
+        public PaymentItemDTO PaymentItem { get; set; }
+    }
+}
